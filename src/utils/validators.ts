a600b4a31f7bac3ac842cf9669b9948d32d46dbf
Selton Jeffeson <selton.jeffeson@catskillet.com>
2022-09-10 17:51:08 -0300

    feat: adjust on validators

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -7,7 +7,7 @@ export const email = (email: string): boolean | string => {
   const regex = new RegExp(
     "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])"
   );
-  return regex.test(email) ? true : "Digite um email válido.";
+  return regex.test(email) || "Digite um email válido.";
 };
 
 export const cpf = (cpf: string): boolean | string => {
